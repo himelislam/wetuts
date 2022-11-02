f60f9ecd0887721671b6159d9522e19479419a55
himelislam <himel7100@gmail.com>
2022-11-02 14:05:46 +0600

    first commit

---
+++ wetuts.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name:       We Tuts
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A Basic Plugin Journey Beggins now
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Himel Islam
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages 
+ */
