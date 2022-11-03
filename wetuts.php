2fd7e694fc7993e290498399de288fab5f7f6b67
himelislam <himel7100@gmail.com>
2022-11-04 01:12:23 +0600

    plugin header

--- wetuts.php
+++ wetuts.php
@@ -8,7 +8,7 @@
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Himel Islam
- * Author URI:        https://author.example.com/
+ * Author URI:        https://himelislam.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
