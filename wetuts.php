25bee99c383e68dc7b9f475bb524f6ab0b73f3e9
himelislam <himel7100@gmail.com>
2022-11-05 22:16:35 +0600

    we tuts

--- wetuts.php
+++ wetuts.php
@@ -15,3 +15,78 @@
  * Text Domain:       my-basics-plugin
  * Domain Path:       /languages 
  */
+/**
+ * Plugin Name:       We Tuts
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A Basic Plugin Journey Beggins now
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Himel Islam
+ * Author URI:        https://himelislam.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages 
+ */
+/**
+ * Plugin Name:       We Tuts
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A Basic Plugin Journey Beggins now
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Himel Islam
+ * Author URI:        https://himelislam.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages 
+ */
+/**
+ * Plugin Name:       We Tuts
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A Basic Plugin Journey Beggins now
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Himel Islam
+ * Author URI:        https://himelislam.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages 
+ */
+/**
+ * Plugin Name:       We Tuts
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A Basic Plugin Journey Beggins now
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Himel Islam
+ * Author URI:        https://himelislam.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages 
+ */
+/**
+ * Plugin Name:       We Tuts
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A Basic Plugin Journey Beggins now
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Himel Islam
+ * Author URI:        https://himelislam.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages 
+ */
