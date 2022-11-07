b2a0f481c23bbb69e6313ca84c70ea7908602611
himelislam <himel7100@gmail.com>
2022-11-07 20:30:22 +0600

    filter hook

--- wetuts.php
+++ wetuts.php
@@ -19,10 +19,24 @@
  function wetuts_seo_tags (){
     ?>
     <!--wetuts seo plugins--->
-    <meta name="description" content="Himel Islam | Web application developer">
-    <meta name="keywords" content="php, ajax, jQuery, php5, wordpress">
+    <meta name="description" content="Himel Islam | Web application developer"/>
+    <meta name="keywords" content="php, ajax, jQuery, php5, wordpress"/>
     <!--wetuts seo plugins--->
     <?php    
  }
 
  add_action( 'wp_head', 'wetuts_seo_tags');
+
+ function wetuts_wp_footer(){
+    echo '<h1>Hello We buddy<h1/>';
+ }
+
+ add_action( 'wp_footer', 'wetuts_wp_footer', 9999);
+
+ function wetuts_author_bio( $content ){
+    var_dump($content);
+
+    return $content . 'Hello Vai Brothers';
+ }
+
+ add_filter('the_content', 'wetuts_author_bio');
